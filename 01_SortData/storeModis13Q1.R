3ae861dea586666b698f62270baf2be7aa26e64d
Ruben Soares Lapa Remelgado <rus14jh@uni-wuerzburg.eu>
2017-06-29 21:33:53 +0200

    updated function

--- 01_SortData/storeModis13Q1.R
+++ 01_SortData/storeModis13Q1.R
@@ -1,6 +1,6 @@
-#---------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------#
 # Description
-#---------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------#
 # Stores MODIS 13Q1 data into a standardized data structure.
 # Retrieves bands for:
 # - NDVI
@@ -13,19 +13,20 @@
 # oPath: parent path where files will be stored
 # delete: Should the hdf file be deleted after usage? Default is FALSE
 
-#---------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------#
 
-storeModis13Q1 <- function(iPath, oPath, delete=F) {
-#---------------------------------------------------------------------------------------------#
+storeModis13Q1 <- function(iPath, oPath, vi=c('ndvi', 'evi'), delete=F) {
+#---------------------------------------------------------------------------------------------------------#
 
   # check variables
-  if (!exists('iPath')) {stop('error: input path missing')}
-  if (!exists('oPath')) {stop('error: output path missing')}
+  if (!exists('iPath')) {stop('error: input path missing')} else {iPath <- file.path(iPath)}
+  if (!exists('oPath')) {stop('error: output path missing')} else {oPath <- file.path(oPath)}
   if (dir.exists(iPath)) {file.path(hdfPath)} else {stop('error: input data path not found')}
   if (dir.exists(oPath)) {file.path(hdfPath)} else {stop('error: output data path not found')}
-  if (!delete%in%c(TRUE, FALSE)) {stop('error: "delete" is not a valid logical argument')}
+  if (min(vi%in%c('ndvi', 'evi'))==0) {stop('error: "vi" argument not valid (use "ndvi", "evi" or both)')}
+  if (!is.logical(delete)) {stop('error: "delete" is not a valid logical argument')}
   
-#---------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------#
 
   # check for and load packages
   pkgTest <- function(x){
@@ -33,45 +34,58 @@ storeModis13Q1 <- function(iPath, oPath, delete=F) {
       install.packages(x,dep=TRUE)
       if(!require(x,character.only = TRUE)) stop("Package not found")
     }
-    library(x)
   }
-  pkgTest("raster")
   pkgTest("gdalUtils")
   
-#---------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------#
   
   # list hdf files
   hdf.ls <- list.files(iPath, '.hdf', full.names=T)
   
   # check for existing collections and build file structure
   col <- as.character(sapply(hdf.ls, function(x) {strsplit(basename(x), '[.]')[[1]][4]}))
+  tiles <- as.character(sapply(hdf.ls, function(x) {strsplit(basename(x), '[.]')[[1]][3]}))
+  prod <- as.character(sapply(hdf.ls, function(x) {strsplit(basename(x), '[.]')[[1]][1]}))
   uc <- unique(col) # unique collections
-  for (u in 1:length(uc)) {
-    path <- paste0(path, '/', uc[u], '/') # collection directory
+  for (c in 1:length(uc)) {
+    path <- paste0(oPath, '/', uc[c], '/') # collection directory
     if (!dir.exists(path)) {dir.create(path)}
-    path <- paste0(path, 'QA/') # quality data
-    if (!dir.exists(path)) {dir.create(path)}
-    path <- paste0(path, 'VI/') # vegetation indices
-    if (!dir.exists(path)) {dir.create(path)}
+    up <- unique(prod[which(col==uc[c])]) # unique products
+    for (p in 1:length(up)) {
+      ut <- unique(tiles[which(col==uc[c] & prod==up[p])]) # unique tiles
+      path <- paste0(oPath, '/', uc[c], '/', up[p], '/')
+      if (!dir.exists(path)) {dir.create(path)} # product path
+      for (t in 1:length(ut)) {
+        tpath <- paste0(oPath, '/', uc[c], '/', up[p], '/', ut[t], '/')
+        if (!dir.exists(tpath)) {dir.create(tpath)} # create tile path
+        path <- paste0(tpath, '/QA/') # quality data
+        if (!dir.exists(path)) {dir.create(path)}
+        path <- paste0(tpath, '/VI/') # vegetation indices
+        if (!dir.exists(path)) {dir.create(path)}
+      }
+    }
   }
-  
-#---------------------------------------------------------------------------------------------#  
+    
+#---------------------------------------------------------------------------------------------------------#  
   
   # extract files
-  for (h in 1:hdf.ls) {
+  for (h in 1:length(hdf.ls)) {
     bname <- strsplit(basename(hdf.ls[h]), '.hdf')[[1]][1] # base file name
-    oname <- paste0(oPath, '/', col[h], '/VI/', bname, '.ndvi.tif')  
-    gdal_translate(hdf.ls[h], oname, sd_index=1) # ndvi
-    oname <- paste0(oPath, '/', col[h], '/VI/', bname, '.evi.tif')  
-    gdal_translate(hdf.ls[h], oname, sd_index=2) # evi
-    oname <- paste0(oPath, '/', col[h], '/QA/', bname, '.doa.tif')  
-    gdal_translate(hdf.ls[h], oname, sd_index=11) # day of acquisition
-    oname <- paste0(oPath, '/', col[h], '/QA/', bname, '.prel.tif')  
-    gdal_translate(hdf.ls[h], oname, sd_index=12) # pixel reliability
+    if ('ndvi'%in%vi) {
+      oname <- paste0(oPath, '/', col[h], '/', prod[h], '/', tiles[h], '/VI/', bname, '.ndvi.tif')  
+      gdalUtils::gdal_translate(hdf.ls[h], oname, sd_index=1, verbose=F) # ndvi
+    }
+    if ('evi'%in%vi) {
+      oname <- paste0(oPath, '/', col[h], '/', prod[h], '/', tiles[h], '/VI/', bname, '.evi.tif')  
+      gdalUtils::gdal_translate(hdf.ls[h], oname, sd_index=2, verbose=F) # evi
+    }
+    oname <- paste0(oPath, '/', col[h], '/', prod[h], '/', tiles[h], '/QA/', bname, '.doa.tif')  
+    gdalUtils::gdal_translate(hdf.ls[h], oname, sd_index=11, verbose=F) # day of acquisition
+    oname <- paste0(oPath, '/', col[h], '/', prod[h], '/', tiles[h], '/QA/', bname, '.prel.tif')  
+    gdalUtils::gdal_translate(hdf.ls[h], oname, sd_index=12, verbose=F) # pixel reliability
+    if(delete) {file.remove(hdf.ls[h])}
   }
   
-  if(delete) {file.remove(hdf.ls[h])}
-  
-#---------------------------------------------------------------------------------------------#  
+#---------------------------------------------------------------------------------------------------------#  
   
 }
